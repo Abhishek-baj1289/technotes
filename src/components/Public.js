efccdbccb65e89907c962d3b617a8d513cc17776
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-03-10 13:13:15 +0530

    commit

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,18 +4,18 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Bajpai' Management System!</span></h1>
+                <h1>Welcome to <span className="nowrap">HRM Management System!</span></h1>
             </header>
             <main className="public__main">
                 <p>Located in Gurgaon's most superluminous location, come to use and name a problem, and we'll have a solution<sup>T&C apply</sup></p>
                 <address className="public__addr">
                     Abhishek Bajpai<br />
-                    8008135 Gurugram<br />
-                    Haryana, Bharat<br />
+                    122001 Gurugram<br />
+                    Haryana, India<br />
                     <a href="tel: 916942069420">965855855854</a>
                 </address>
                 <br />
-                <p>Owner: Abhishek Bajpai</p>
+                <p>Owner: Owner</p>
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
